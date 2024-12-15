417a3d7467abe2656ae289c4a3b9beec059bb1fb
yifengou <2772674957@qq.com>
2024-12-15 21:42:45 +0800

    重复字符串的最小序

--- A1214/src/stack/RemoveDuplicateLettersDemo1.java
+++ A1214/src/stack/RemoveDuplicateLettersDemo1.java
@@ -1,5 +1,6 @@
 package stack;
 
+import java.util.Stack;
 public class RemoveDuplicateLettersDemo1 {
     /*
     * 给你一个字符串 s ，请你去除字符串中重复的字母，使得每个字母只出现一次。需保证 返回结果的字典序
@@ -11,6 +12,41 @@ public class RemoveDuplicateLettersDemo1 {
     输出："acdb"
     * */
     public String removeDuplicateLetters(String s) {
-
+        //1.去掉重复字母做准备，存储记录字符数量。
+        int[] arr=new int[26];
+        for (char c : s.toCharArray()) {
+            arr[c-'a']++;
+        }
+        //StringBuilder ans=new StringBuilder(26);
+        Stack<Character> ans=new Stack<>();//用队列也可
+        boolean[] inAns =new boolean[26];
+        for (char c : s.toCharArray()) {
+            arr[c-'a']--;
+            if(ans.isEmpty()||arr[c-'a']==1){
+                ans.push(c);
+                inAns[c-'a']=true;
+            }
+            if(inAns[c-'a']){
+               continue;
+            }
+            char ch=ans.peek();
+            if(c-ch<0){
+                if(arr[ch-'a']>0){
+                    inAns[ch-'a']=false;
+                    ans.pop();
+                }
+                ans.push(c);
+                inAns[c-'a']=true;
+            }
+            else if(c-ch>0){
+                ans.push(c);
+                inAns[c-'a']=true;
+            }
+        }
+        String str="";
+        while (!ans.isEmpty()){
+           str=ans.pop()+str;
+        }
+        return str;
     }
 }
