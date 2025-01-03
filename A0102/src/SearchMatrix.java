58a8c825115301454c65fdeebd604346c4743ab2
yifengou <2772674957@qq.com>
2025-01-03 23:49:29 +0800

    搜索二维矩阵

--- A0102/src/SearchMatrix.java
+++ A0102/src/SearchMatrix.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class SearchMatrix {
+public class SearchMatrix {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int r = matrix.length;
+        int c = matrix[0].length;
+        for (int i = 0; i < r; i++) {
+            if (matrix[i][c - 1] >= target){//注意判断条件
+                for (int m = c - 1; m >= 0; m--) {
+                    if (matrix[i][m] == target)
+                        return true;
+                }
+            }
+        }
+        return false;
+    }
 }
