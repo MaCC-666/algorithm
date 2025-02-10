e600df534fa621e0eb12246daeaaedd32ad458fb
yifengou <2772674957@qq.com>
2025-02-10 23:37:14 +0800

    链表算法

--- A0209/src/SwapPairs.java
+++ A0209/src/SwapPairs.java
@@ -2,13 +2,24 @@ import LinkedList.ListNode;
 
 public class SwapPairs {
     public ListNode swapPairs(ListNode head) {
-        //1、递归
-        if(head==null||head.next==null)
-            return head;
-        ListNode swap=head.next;
-        //ListNode newHead=swap.next;
-        head.next=swapPairs(swap.next);
-        swap.next=head;
-        return swap;
+        ListNode dummy = new ListNode(0);
+        dummy.next=head;
+        ListNode newHead=dummy;
+        while (head!=null&&head.next!=null){
+            ListNode swap=head.next;
+            head.next=swap.next;
+            newHead.next=swap;
+            swap.next=head;
+            newHead=head;
+            head=head.next;
+        }
+        return dummy.next;
     }
+    //1、递归
+//        if(head==null||head.next==null)
+//            return head;
+//        ListNode swap=head.next;
+//        head.next=swapPairs(swap.next);
+//        swap.next=head;
+//        return swap;
 }
